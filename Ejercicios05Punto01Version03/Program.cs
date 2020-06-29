dc558f0ab60e6fbf50c018930ba573915dbe226f
Carlos Enrique Cimino <ciminoce@yahoo.com.ar>
2020-06-29 19:08:04 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicios05Punto01Version03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejercicios05Punto01Version03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title="Ejemplo con ciclo repetitivo For";
+            var suma = 0;
+            for (int contador = 0; contador < 4; contador++)
+            {
+                Console.Write($"Ingrese el número {contador+1}:");
+                var nro = int.Parse(Console.ReadLine());
+                suma += nro;
+            }
+            Console.Write($"La suma es {suma}");
+        }
+    }
+}
